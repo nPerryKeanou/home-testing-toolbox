053aca8ca938cb44004a4fb6fac0fc7db20f15b8
nperryk <nperrykeanou@gmail.com>
2025-09-01 20:44:34 +0200

    feat: ajout des fonction choose_email et pause dans helpers.sh

--- scripts/email.sh
+++ scripts/email.sh
@@ -1,1 +1,12 @@
 #Tests liés aux emails.
+
+# # =============================
+# # GÉNÉRATION EMAIL UNIQUE
+# # =============================
+generate_email(){
+  local timestamp=$(date +%s)
+  local random=$RANDOM
+  EMAIL="user_${timestamp}_${random}@example.com"
+  echo "$EMAIL" >> "$EMAIL_HISTORY_FILE"
+  echo "-> Email généré : $EMAIL"
+}
--- utils/helpers.sh
+++ utils/helpers.sh
@@ -1,1 +1,16 @@
 #Centralisations de fonctions utiles(par ex: choose_email, gestion de fichiers tmp, affichage menu,...)
+
+check_error(){
+  local response="$1"
+  local step="$2"
+  if echo "$response" | grep -qi "error"; then
+    echo "* Erreur à l'étape $step: $response"
+  else
+    echo "* Étape $step réussie"
+  fi
+}
+
+
+pause(){
+  read -p "-> Appuie sur [Entrée] pour continuer..."
+}
